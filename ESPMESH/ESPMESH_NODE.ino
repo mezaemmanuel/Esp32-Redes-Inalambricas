11b3b7bef0d1502f1df279c52d0443946ce46e8d
Emmanuel Meza <68042653+mezaemmanuel@users.noreply.github.com>
2023-01-10 08:19:14 -0600

    Update ESPMESH_NODE.ino

--- ESPMESH/ESPMESH_NODE.ino
+++ ESPMESH/ESPMESH_NODE.ino
@@ -5,50 +5,60 @@ siendo asi solo un nodo para lectura de datos y trasmision a un master
 */
 //***MESH**//
 #include <painlessMesh.h>
-#define   MESH_PREFIX     "SSID"          //Nombre de la red a creear
-#define   MESH_PASSWORD   "PASSWORD"     // Contraseña de la red
-#define   MESH_PORT       5555           // puerto de Conexion 
-
-Scheduler userScheduler;
+#define   MESH_PREFIX     "SM_Espanta"
+#define   MESH_PASSWORD   "SM_Espanta_2023"
+#define   MESH_PORT       5555
 painlessMesh  mesh;
+Scheduler userScheduler;
 
-void sendMessage() ; 
-Task taskSendMessage( TASK_SECOND * 1 , TASK_FOREVER, &sendMessage );// aqui configuramos la frecuencia con la que enviaremos los mensajes
+uint8_t WIFIChannel=13;
+void sendMessage() ; // Prototype so PlatformIO doesn't complain
 
+Task taskSendMessage( TASK_SECOND * 5 , TASK_FOREVER, &sendMessage );
+String msg="";
 void sendMessage() {
-  
- String Mensaje = "Hola";
-  Serial.println("El mensaje que voy a enviar es : " + Mensaje);
-  Serial.println(Mensaje);
-  mesh.sendBroadcast(Mensaje);
-  //mesh.sendSingle(nodeidMaster, Mensaje);
+  //msg = "Hi from node1: ";
+  //msg += mesh.getNodeId();
+  mesh.sendBroadcast( msg );
+//  taskSendMessage.setInterval( random( TASK_SECOND * 1, TASK_SECOND * 5 ));
 }
 
-// Needed for painless library-----------opcional.
 void receivedCallback( uint32_t from, String &msg ) {
   Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
 }
+
 void newConnectionCallback(uint32_t nodeId) {
     Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
 }
+
 void changedConnectionCallback() {
   Serial.printf("Changed connections\n");
 }
+
 void nodeTimeAdjustedCallback(int32_t offset) {
     Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
 }
+
+
+
 void setup() {
   Serial.begin(115200);
-  mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT, WIFI_AP_STA, 13);
+
+  mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT, WIFI_AP_STA, WIFIChannel);
   mesh.onReceive(&receivedCallback);
-
-  taskSendMessage.setInterval( TASK_SECOND * 10);
-  userScheduler.addTask(taskSendMessage);
+  userScheduler.addTask( taskSendMessage );
   taskSendMessage.enable();
-
+  Serial.println("Mi indentificador de nodo en es:" + String(mesh.getNodeId()));
 }
 
 void loop() {
-   mesh.update();
-
+  // it will run the user scheduler as well
+  /*int NumNodes = mesh.getNodeList().size();
+  Serial.println(NumNodes);*/
+  if (Serial.available()){
+    msg = Serial.readString();  
+    Serial.println("Sending:" + msg);  
+    mesh.sendBroadcast(msg);
+  }
+  mesh.update();
 }
